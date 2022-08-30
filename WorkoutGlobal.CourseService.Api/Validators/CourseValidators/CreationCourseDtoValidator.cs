68b01a8fdbf13b553cbce5e74f29959d0d836554
MagzyCode <mihailkazarevich@mail.ru>
2022-08-30 15:24:54 +0300

    Create validators.

---
+++ WorkoutGlobal.CourseService.Api/Validators/CourseValidators/CreationCourseDtoValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using WorkoutGlobal.CourseService.Api.Dto;
+
+namespace WorkoutGlobal.CourseService.Api.Validators.CourseValidators
+{
+    /// <summary>
+    /// Validator for CreationCourseDto model.
+    /// </summary>
+    public class CreationCourseDtoValidator : AbstractValidator<CreationCourseDto>
+    {
+        /// <summary>
+        /// Ctor for validator.
+        /// </summary>
+        public CreationCourseDtoValidator()
+        {
+            RuleFor(course => course.Name)
+                .NotEmpty()
+                .Length(5, 200);
+
+            RuleFor(course => course.Description)
+                .NotEmpty()
+                .Length(5, 500);
+
+            When(course => course.Logo is not null, () =>
+            {
+                RuleFor(course => course.Logo)
+                    .NotEmpty();
+            });
+
+            RuleFor(course => course.CreatorId)
+                .NotEmpty();
+        }
+    }
+}
